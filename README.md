529637bb76ed60d12e7687905747d9780e953ad0
Patrik Jonell <jonell.patrik@gmail.com>
2014-08-10 00:15:15 +0200

    fixed twitter posting, did code clean-up, and prepare for arduino

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ sends out messages through twitter using python, stuff like "water the plants, p
 How to install?
 pip install -r requirements.txt
 
-How to get access to twitter API? 
-Wee place your .tweetrc file with the appropriate configs in the right place (home folder)
+Config file:
+CONSUMER_KEY      = "xxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN      = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_SECRET     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SERIAL_PORT       = "/dev/tty.usbX"
 
 How to run?
-Easy: python2 (so install with pip2)
-Currently ready for python3 but configparser stuff changed quite a bit and its a hassle, so untested.
+Easy: 
+python ewok_twitter.py
--- ewok_twitter.py
+++ ewok_twitter.py
@@ -2,80 +2,30 @@
 #This file is modified from the https://github.com/bear/python-twitter
 #Kudos go all to bear and the makers of this lib, all lib/wrapper related things remain with same LICENSE, please see: https://github.com/bear/python-twitter for LICENSE
 
-import ConfigParser
 import getopt
 import os
 import sys
-import twitter
+from twitter import *
+import config
+import serial
 
-def GetConsumerKeyEnv():
-  return os.environ.get("TWEETUSERNAME", None)
-def GetConsumerSecretEnv():
-  return os.environ.get("TWEETPASSWORD", None)
 
-def GetAccessKeyEnv():
-  return os.environ.get("TWEETACCESSKEY", None)
-
-def GetAccessSecretEnv():
-  return os.environ.get("TWEETACCESSSECRET", None)
-class TweetRc(object):
-  def __init__(self):
-    self._config = None
-
-  def GetConsumerKey(self):
-    return self._GetOption('consumer_key')
-
-  def GetConsumerSecret(self):
-    return self._GetOption('consumer_secret')
-
-  def GetAccessKey(self):
-    return self._GetOption('access_key')
-
-  def GetAccessSecret(self):
-    return self._GetOption('access_secret')
-
-  def _GetOption(self, option):
-    try:
-      return self._GetConfig().get('Tweet', option)
-    except:
-      return None
-
-  def _GetConfig(self):
-    if not self._config:
-      self._config = ConfigParser.ConfigParser()
-      self._config.read(os.path.expanduser('~/.tweetrc'))
-    return self._config
 
 def main(message):
-  try:
-    shortflags = 'h'
-    longflags = ['help', 'consumer-key=', 'consumer-secret=', 
-                 'access-key=', 'access-secret=', 'encoding=']
-    opts, args = getopt.gnu_getopt(sys.argv[1:], shortflags, longflags)
-  except getopt.GetoptError:
-    print("No file exception")
-  consumer_keyflag = None
-  consumer_secretflag = None
-  access_keyflag = None
-  access_secretflag = None
-  encoding = None
-  rc = TweetRc()
-  consumer_key = consumer_keyflag or GetConsumerKeyEnv() or rc.GetConsumerKey()
-  consumer_secret = consumer_secretflag or GetConsumerSecretEnv() or rc.GetConsumerSecret()
-  access_key = access_keyflag or GetAccessKeyEnv() or rc.GetAccessKey()
-  access_secret = access_secretflag or GetAccessSecretEnv() or rc.GetAccessSecret()
-  if not consumer_key or not consumer_secret or not access_key or not access_secret:
-    PrintUsageAndExit()
-  api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-                    access_token_key=access_key, access_token_secret=access_secret,
-                    input_encoding=encoding)
-  try:
-    status = api.PostUpdate(message)
-  except UnicodeDecodeError:
-    print("Your message could not be encoded.  Perhaps it contains non-ASCII characters?")
-    print("Try explicitly specifying the encoding with the --encoding flag")
-    sys.exit(2)
-  print("%s just posted: %s" % (status.user.name, status.text))
+  twitter_api = Twitter(auth=OAuth(config.ACCESS_TOKEN, config.ACCESS_SECRET, config.CONSUMER_KEY, config.CONSUMER_SECRET))
+  ser = serial.Serial(port=config.SERIAL_PORT, baudrate=19200, timeout=0, writeTimeout=0) 
+  
+  while True:
+    data = self.ser.readline(1)                
+    if data:
+      # check how the plants are feeling.. IF they are under threshold X, then tweet!
+      plant_name = "Mr X"
+      #twitter_api.statuses.update(status=message + "\n Yours sincerely " + plant_name)
+    time.sleep(1)
+  
+  
+
+  
 
 if __name__ == "__main__":
-  main("hello, patrik put here the message to send out!")
+  main("Help! I'm dying!")
